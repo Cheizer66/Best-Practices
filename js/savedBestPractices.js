3f260004459324f18e5a82ce6460f77ef4713345
Cheizer <52860299+Cheizer66@users.noreply.github.com>
2020-12-06 11:49:24 +0100

    Add files via upload

--- js/savedBestPractices.js
+++ js/savedBestPractices.js
@@ -7,11 +7,12 @@ let allSavedBestPractices = [];
 function saveBestPractice() {
   let savedBestPractice = state;
   allSavedBestPractices.push(...state);
-  const htmlSavedBestPractice = `<div class="popupSaved__container">
+  saveContainer.innerHTML = "";
+  const htmlSavedBestPractice = `<div class="popupSaved__text">
   <h3 class="heading-saved">Vraag</h3>
-  <div class="popupSaved__question par__question-saved">${state[3]}</div>
-  <h3 class="heading-saved">Antwoord</h3>
-  <div class="popupSaved__question par__answer-saved">${state[4]}</div>
+  <div class="par__question-saved">${state[3]}</div>
+  <button class="btn__showAnswer" onclick="showAnswer()">Antwoord</button>
+  <div id="par__answer-saved">${state[4]}</div>
   <div class="savedLine"></div>
 </div>`;
   saveContainer.insertAdjacentHTML("afterbegin", htmlSavedBestPractice);
@@ -21,4 +22,14 @@ function saveBestPractice() {
 document.querySelector(".saveAnswersText").addEventListener("click", saveBestPractice);
 
 // QUERY SELECTORS
-const saveContainer = document.querySelector(".popupSaved__content");
+const saveContainer = document.querySelector(".popupSaved__text");
+
+// SHOW ANSWER
+function showAnswer() {
+  const answer = document.getElementById("par__answer-saved");
+  if (answer.style.display === "none") {
+    answer.style.display = "block";
+  } else {
+    answer.style.display = "none";
+  }
+}
